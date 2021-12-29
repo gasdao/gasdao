1ef7d71248a57bdc2ad5b975448b1f36922b5c44
gasdao <96785333+gasdao@users.noreply.github.com>
2021-12-28 21:01:27 -0500

    Update GasDaoGovernor.sol

--- contracts/GasDaoGovernor.sol
+++ contracts/GasDaoGovernor.sol
@@ -25,7 +25,7 @@ contract GasDaoGovernor is Governor, GovernorProposalThreshold, GovernorCounting
     }
 
     function proposalThreshold() public pure override returns (uint256) {
-        return 100_000_000e18;
+        return 1_000_000_000e18;
     }
 
     function quorumDenominator() public pure virtual override returns (uint256) {
